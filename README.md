d74232406b0ce1fc457b6b68bb8e595c57bf8aed
rettar <rettar.v@gmail.com>
2018-02-12 19:18:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# NullPointerException
+## Install
+
+```
+$ cd ./ODN-CLIENT
+$ cd ./src/plugins/tweet
+$ git clone https://github.com/rettar5/OCPT_NullPointerException.git nullPointerException
+$ npm run load:build
+```
---
+++ nullPointerException.ts
@@ -0,0 +1,34 @@
+import { OdnTweetData, OdnTweets } from "../../../odnTweets"
+import { OdnPlugins, OdnPluginResultData } from "../../../odnPlugins";
+import {Log, OdnUtils} from "../../../odnUtils";
+
+export class NullPointerException {
+  constructor(private tweetData: OdnTweetData, private fullName: string) {}
+
+  /**
+   * プラグインのメイン処理を実行
+   *
+   * @param {(isProcessed?: boolean) => void} finish
+   */
+  run(finish: (isProcessed?: boolean) => void) {
+    const tweets = new OdnTweets(this.tweetData.accountData);
+    tweets.text = "@" + this.tweetData.user.screenName + " ｶﾞｯ";
+    tweets.targetTweetId = this.tweetData.idStr;
+
+    // ツイートを投稿
+    tweets.postTweet((isSuccess) => {
+      tweets.likeTweet();
+      finish();
+    });
+  }
+
+  /**
+   * プラグインを実行するかどうか判定
+   *
+   * @param {OdnTweetData} tweetData
+   * @returns {boolean}
+   */
+  static isValid(tweetData: OdnTweetData): boolean {
+    return tweetData.text.match(/^(ぬるぽ|ヌルポ|ﾇﾙﾎﾟ)$/gi) ? true : false;
+  }
+}
